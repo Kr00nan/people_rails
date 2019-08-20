42c3f61ec8947fa524a00591f1837c272cdb89f3
Michael Kroon <kroonology@gmail.com>
2019-08-20 14:59:35 -0600

    added index, show, new methods

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,10 +1,19 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
   end
 
   def show
+    @person = Person.find(params[:id])
   end
 
   def new
+    @person = Person.new
+  end
+
+  def update
+  end
+
+  def delete
   end
 end
